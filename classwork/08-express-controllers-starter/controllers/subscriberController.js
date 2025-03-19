3e832947811447e50b8c898de02947a851e1e6e3
Andres Hung <2018118240@ub.edu.bz>
2025-03-19 12:12:21 -0600

    Change models to use database

--- classwork/08-express-controllers-starter/controllers/subscriberController.js
+++ classwork/08-express-controllers-starter/controllers/subscriberController.js
@@ -12,16 +12,28 @@ export const getSignup = (req, res) => {
   });
 };
 
-export const postSignup = (req, res) => {
+export const postSignup = async (req, res) => {
   const { name, email } = req.body;
   if (!name || !email) {
     return res.status(400).send('Name and email are required.');
   }
 
-  const newSignup = addSignup(name, email);
-  res.render('thankyou', { title: 'Thank You', ...newSignup });
+  try {
+    const newSignup = await addSignup(name, email);
+    res.render('thankyou', { title: 'Thank You', ...newSignup });
+  } catch (error) {
+    if (error.code === '23505') {
+      return res.status(400).send('This email is already registered.');
+    }
+    res.status(500).send('An error occurred during signup.');
+  }
 };
 
-export const getSignups = (req, res) => {
-  res.json(getAllSignups());
+export const getSignups = async (req, res) => {
+  try {
+    const signups = await getAllSignups();
+    res.json(signups);
+  } catch (error) {
+    res.status(500).send('An error occurred fetching signups.');
+  }
 };
--- classwork/08-express-controllers-starter/models/subscriberModel.js
+++ classwork/08-express-controllers-starter/models/subscriberModel.js
@@ -1,11 +1,24 @@
-let signups = [];
+import { query } from '../config/db.js';
 
-export const getAllSignups = () => {
-  return signups;
+export const getAllSignups = async () => {
+  try {
+    const result = await query('SELECT username, email FROM signups');
+    return result.rows;
+  } catch (error) {
+    console.error('Errory fetching signups:', error);
+    throw error;
+  }
 };
 
-export const addSignup = (name, email) => {
-  const newSignup = { name, email };
-  signups.push(newSignup);
-  return newSignup;
+export const addSignup = async (username, email) => {
+  try {
+    const result = await query(
+      'INSERT INTO signups (username, email) VALUES ($1, $2) RETURNING *',
+      [username, email]
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.error('Error adding signups:', error);
+    throw error;
+  }
 };
